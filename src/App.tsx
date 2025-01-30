f33ce42210166242453f295b3c7e06dcd1a6b33b
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-30 13:34:09 +0700

    List rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,49 @@ import ProfileCard from './components/ProfileCard.tsx'
 import Footer from './components/Footer.tsx'
 import './App.css'
 
+type student = {
+  id: number,
+  name: string,
+  email: string,
+  age: number
+}
+
+const students: student[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@me.com",
+    age: 25
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "janedoe@me.com",
+    age: 30
+  },
+  {
+    id: 3,
+    name: "Bob Doe",
+    email: "bob@me.com",
+    age: 35
+  }
+]
 function App() {
 
   return (
     <>
       <Navbar />
-      <ProfileCard email="johndoe@me.com" />
+      {
+        students.map((student) => {
+          return (
+            <ProfileCard key={student.id} name={student.name} email={student.email} age={student.age} />
+          )
+        })
+      }
+
+      {/* <ProfileCard email="johndoe@me.com" />
       <ProfileCard name="Alice Doe" email="alice@me.com" age={30} />
-      <ProfileCard name="Kevin Doe" email="kevin@me.com" age={25} />
+      <ProfileCard name="Kevin Doe" email="kevin@me.com" age={25} /> */}
       <Footer />
     </>
   )
