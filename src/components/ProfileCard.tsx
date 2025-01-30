d93fe67498f3e7c43e4075bc9896e4e5364dde97
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-30 13:07:15 +0700

    Components

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,11 @@
+const ProfileCard = () => {
+    return (
+        <div className="card">
+            <p>Name : Rizky</p>
+            <p>Email : rizky@gmail.com</p>
+            <p>Age : 16</p>
+        </div>
+    )
+}
+
+export default ProfileCard
